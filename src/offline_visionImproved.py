ece03d0cb807e60031b0d4139f8f01d12147f7e2
Philippe Thomas <philippe.thomas@dfki.de>
2025-01-18 17:57:36 +0100

    Minor changes

--- src/offline_visionImproved.py
+++ src/offline_visionImproved.py
@@ -1,10 +1,23 @@
 from vllm import SamplingParams
 from vllm.assets.image import ImageAsset
+import argparse
 
-from other.visionModels import Qwen2VL
+from other.visionModels import *
 
-llava_runner = Qwen2VL(model_name = "Qwen/Qwen2-VL-7B-Instruct", model_path = "/netscratch/thomas/models/") #Load the model
-llm, prompt, stop_token_ids = llava_runner.query("What is shown in the image?")
+parser = argparse.ArgumentParser(description="Run a specific LLM model.")
+parser.add_argument("--model", type=str, required=True, help="Name of the model to use (LLAVA, LLAVANext, Qwen2VL)")
+
+if parser.model == "QWEN2VL":
+    runner = Qwen2VL()
+
+elif parser.model == "LLAVA":
+    runner = LLAVA()
+
+elif parser.model == "LLAVANEXT":
+    runner = LLAVANext()
+
+#llava_runner = Qwen2VL(model_name = "Qwen/Qwen2-VL-7B-Instruct", model_path = "/netscratch/thomas/models/") #Load the model
+llm, prompt, stop_token_ids = runner.query("What is shown in the image?")
 
 sampling_params = SamplingParams(temperature=0.2,
                                      max_tokens=64,
